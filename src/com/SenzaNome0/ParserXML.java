fac8ffbbbeb7cc5dac517753ba394dfe358dfc82
Nicola Plaka <nicola.plaka2004@gmail.com>
2022-06-03 19:48:26 +0200

    ParseXML getGrafo changed into Linkedlist

--- src/com/SenzaNome0/ParserXML.java
+++ src/com/SenzaNome0/ParserXML.java
@@ -6,6 +6,10 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class ParserXML {
     XMLInputFactory xmlif = null;
@@ -14,8 +18,8 @@ public class ParserXML {
         xmlif = XMLInputFactory.newInstance();
     }
 
-    public Grafo getGrafo(String filename) throws XMLStreamException {
-        Grafo grafo = null;
+    public LinkedList<ArrayList<Integer>> getGrafo(String filename) throws XMLStreamException {
+        LinkedList<ArrayList<Integer>> collegamenti = new LinkedList<>();
         XMLStreamReader xmlr = null;
         try {
             xmlr = xmlif.createXMLStreamReader(filename, new FileInputStream(filename));
@@ -23,14 +27,11 @@ public class ParserXML {
             System.out.println("Errore nell'inizializzazione del reader:");
             System.out.println(e.getMessage());
         }
+        //mappa di lista di edge
         while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
             switch (xmlr.getEventType()) { // switch sul tipo di evento
                 case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
-                    grafo.addNodo(new Nodo(Integer.parseInt(xmlr.getAttributeValue(0)),xmlr.getAttributeLocalName(1), Integer.parseInt(xmlr.getAttributeValue(2)), Integer.parseInt(xmlr.getAttributeValue(3)), Integer.parseInt(xmlr.getAttributeValue(4) )));
-                case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
-                    System.out.println("END-Tag " + xmlr.getLocalName()); break;
-                case XMLStreamConstants.COMMENT:
-                    System.out.println("// commento " + xmlr.getText()); break; // commento: ne stampa il contenuto
+                    collegamenti.add(new Nodo(Integer.parseInt(xmlr.getAttributeValue(0)),xmlr.getAttributeLocalName(1), Integer.parseInt(xmlr.getAttributeValue(2)), Integer.parseInt(xmlr.getAttributeValue(3)), Integer.parseInt(xmlr.getAttributeValue(4) )));
                 case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
                     if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
                         System.out.println("-> " + xmlr.getText());
@@ -38,6 +39,6 @@ public class ParserXML {
             }
             xmlr.next();
         }
-        return grafo;
+        return collegamenti;
     }
 }
