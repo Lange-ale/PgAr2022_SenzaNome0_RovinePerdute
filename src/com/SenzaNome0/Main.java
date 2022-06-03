6071a8951d1bd279b5adfd7a07b40f8027db68f5
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-06-03 20:55:06 +0200

    Aggiunti i riferimenti di ogni nodo nel grafo al metodo getGrafo del ParserXML...

--- src/com/SenzaNome0/Main.java
+++ src/com/SenzaNome0/Main.java
@@ -1,13 +1,23 @@
 package com.SenzaNome0;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         ParserXML parserXML = new ParserXML();
         int[] filenames = {5, 12, 50, 200, 2000, 10000};
         for (int i : filenames) {
-            Grafo grafo = parserXML.getGrafo("Copy of PgAr_Map_" + i + ".xml");
-            //initDistances(true);
-            System.out.println(grafo);
-            break; //TODO DEL ME
+            try {
+                Grafo grafo = parserXML.getGrafo("Copy of PgAr_Map_" + i + ".xml");
+                //initDistances(true);
+                System.out.println(grafo);
+                break; // TODO: DEL ME
+            } catch (ParserConfigurationException | IOException | SAXException e) {
+                e.printStackTrace();
+            }
         }
     }
 
