6ea009c900d2a6aec3cbb4a4edaec30729ad11d6
Nicola Plaka <nicola.plaka2004@gmail.com>
2022-06-03 19:32:53 +0200

    ParseXML getGrafo

--- src/com/SenzaNome0/ParserXML.java
+++ src/com/SenzaNome0/ParserXML.java
@@ -1,4 +1,43 @@
 package com.SenzaNome0;
 
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+
 public class ParserXML {
+    XMLInputFactory xmlif = null;
+
+    public ParserXML() {
+        xmlif = XMLInputFactory.newInstance();
+    }
+
+    public Grafo getGrafo(String filename) throws XMLStreamException {
+        Grafo grafo = null;
+        XMLStreamReader xmlr = null;
+        try {
+            xmlr = xmlif.createXMLStreamReader(filename, new FileInputStream(filename));
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del reader:");
+            System.out.println(e.getMessage());
+        }
+        while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
+            switch (xmlr.getEventType()) { // switch sul tipo di evento
+                case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
+                    grafo.addNodo(new Nodo(Integer.parseInt(xmlr.getAttributeValue(0)),xmlr.getAttributeLocalName(1), Integer.parseInt(xmlr.getAttributeValue(2)), Integer.parseInt(xmlr.getAttributeValue(3)), Integer.parseInt(xmlr.getAttributeValue(4) )));
+                case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
+                    System.out.println("END-Tag " + xmlr.getLocalName()); break;
+                case XMLStreamConstants.COMMENT:
+                    System.out.println("// commento " + xmlr.getText()); break; // commento: ne stampa il contenuto
+                case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
+                    if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
+                        System.out.println("-> " + xmlr.getText());
+                    break;
+            }
+            xmlr.next();
+        }
+        return grafo;
+    }
 }
